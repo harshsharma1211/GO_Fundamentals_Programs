b486893acb64d8cf1e09f0321b2d07030cafb897
Shubham_bhatt <harshsharma1211@gmail.com>
2025-03-31 18:05:35 +0530

    Resolve merge conflict in document/main.go

---
+++ document/main.go
@@ -0,0 +1,12 @@
+package main
+
+// "./dog"
+
+func main() {
+
+	h := 7 // variable h stores the value of human_year
+
+	// function Years is called the converted dog year value is printed
+	Years(h)
+
+}
